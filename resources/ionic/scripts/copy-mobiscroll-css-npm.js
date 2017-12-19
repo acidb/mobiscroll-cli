1784d368f864b9bdf223a78a6def3c75936ac2ee
szily20 <szily20@gmail.com>
2017-12-19 15:42:59 +0200

    Use local npm-login instead of git and removed assets copy from copy script.

--- resources/ionic/scripts/copy-mobiscroll-css-npm.js
+++ resources/ionic/scripts/copy-mobiscroll-css-npm.js
@@ -1,8 +1,4 @@
 module.exports = {
-  copyAssets: {
-    src: ['{{SRC}}/assets/images/*'],
-    dest: '{{WWW}}/assets/images'
-  },
   copyMobiscrollCss: {
     src: ['{{ROOT}}/node_modules/@mobiscroll/angular/dist/css/*'],
     dest: '{{WWW}}/lib/mobiscroll/css/'
---
+++ src/npm-login/index.js
@@ -0,0 +1,28 @@
+var ncl = require('./login.js');
+
+module.exports = function (user, pass, email, registry, scope, quotes, configPath) {
+    var finalArgs = ncl.processArguments(user, pass, email, registry, scope, quotes, configPath);
+    var response;
+    var contents;
+    var newContents;
+
+    return new Promise(function (resolve, reject) {
+        ncl.login(finalArgs, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                response = data;
+                ncl.readFile(finalArgs, function (err, data) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        contents = data;
+                        newContents = ncl.generateFileContents(finalArgs, contents, response);
+                        ncl.writeFile(finalArgs, newContents);
+                        resolve();
+                    }
+                });
+            }
+        });
+    });
+};
